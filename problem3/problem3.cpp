dabda483ef329d232503ba7e2231a3d8798a7d87
Claudia Luque <Clau.spring@gmail.com>
2014-06-22 18:56:47 +0100

    adding some functionality

--- problem3/problem3.cpp
+++ problem3/problem3.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 
+#include "problem3.hpp"
+
 // Initializing
 Number::Number(int n)
 : mNumber(n)
@@ -9,30 +11,47 @@ Number::Number(int n)
 }
 
 // Get the factors of a number
-std::vector<int> problem3::getFactors()
+std::vector<int> Number::getFactors(int mNumber)
 {
-   double squareRootValue = floor(sqrt(mNumber));
+   double squareRootValue = floor(sqrt(static_cast<double>(mNumber)));
    
    // Cheching if
-   if(squareRootValue >= 2) 
+   if(squareRootValue >= 2)
    {
        for(int i = 2; i <= squareRootValue; i++)
        {
            if(mNumber % i == 0)
-               mFactors = push(i);
-           else
-               getFactors();
-       }      
+           {
+               mFactors.push_back(i);
+               mNumber = mNumber / i;
+               getFactors(mNumber);
+           }
+       }
    }
+   else
+       primeNumbers.push_back(mNumber);
+
+   return primeNumbers;
 }
 
-int problem3::getLargestPrimeFactor()
+int Number::getLargestPrimeFactor()
 {
+    int maxNumber = 0;
+    
+    for(int i = 0; i < primeNumbers.size() ;i++) 
+    {
+        maxNumber = primeNumbers[i];
         
+        if(maxNumber < primeNumbers[i])
+            maxNumber = primeNumbers[i];
+    }
+    return maxNumber;
 }
-
 
 int main() {
 	
+    Number obj(5);
+    obj.getFactors(12);
+    std::cout<<obj.getLargestPrimeFactor()<<std::endl;
 	return 0;
 }
