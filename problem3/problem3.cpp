c886bd6736769bdbf8b90c06792f016e15a0612f
Claudia Luque <Clau.spring@gmail.com>
2014-06-22 14:22:11 +0100

    adding some changes

--- problem3/problem3.cpp
+++ problem3/problem3.cpp
@@ -12,3 +12,41 @@ private:
 
 
 
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+// Initializing
+Number::Number(int n)
+: mNumber(n)
+{
+}
+
+// Get the factors of a number
+std::vector<int> problem3::getFactors()
+{
+   double squareRootValue = floor(sqrt(mNumber));
+   
+   // Cheching if
+   if(squareRootValue >= 2) 
+   {
+       for(int i = 2; i <= squareRootValue; i++)
+       {
+           if(mNumber % i == 0)
+               mFactors = push(i);
+           else
+               getFactors();
+       }      
+   }
+}
+
+int problem3::getLargestPrimeFactor()
+{
+        
+}
+
+
+int main() {
+	
+	return 0;
+}
