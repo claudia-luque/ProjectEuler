0d8c98bd50f1f33454c75a05d7b268ae04fcab88
Claudia Luque <Clau.spring@gmail.com>
2014-06-22 13:27:01 +0100

    changing name

---
+++ problem3/problem3.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Number
+{
+
+private:
+    int number;
+	
+    std::vector<int> function getFactors();
+    int function getLargestPrimeFactor();
+}
+
+
+
+int main() {
+	
+	return 0;
+}
