302784cf5ccc5241f843579f4e78455cf3f9bf26
Claudia Luque <Clau.spring@gmail.com>
2014-06-22 14:14:05 +0100

    cleaning up

--- problem3/problem3.hpp
+++ problem3/problem3.hpp
@@ -1,18 +1,14 @@
 #include <iostream>
+#include <vector>
 
 class Number
 {
 
 private:
-    int number;
-	
+    int mNumber;
+    std::vector<int> mFactors;
+   	
+    Number(int n);
     std::vector<int> function getFactors();
     int function getLargestPrimeFactor();
-}
-
-
-
-int main() {
-	
-	return 0;
 }
